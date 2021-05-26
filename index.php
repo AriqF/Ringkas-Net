41f930fb631254faf368c4a64761cbcc99a2bc1c
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-05-26 10:22:03 +0700

    create index page

---
+++ index.php
@@ -0,0 +1,299 @@
+<html>
+    <head>
+        <title>AdoptMe | Home</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- icon -->
+        <link rel="icon" type="image/png" href="src/img/adoptMe.png">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+      
+        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+        <!--My css-->
+        <link href="src/css/indexStyle.css" type="text/css" rel="stylesheet"> 
+        <link href="src/css/animation.css" type="text/css" rel="stylesheet"> 
+        
+        <!--Google Fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Benne&family=Overpass&family=Poppins&display=swap" rel="stylesheet">
+
+        <script src="https://kit.fontawesome.com/babb4f3fd7.js" crossorigin="anonymous"></script>
+    </head>
+    <body id="page-top">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top" style="font-family: 'Benne', serif; font-size:22px; letter-spacing:1px">Ringkas.Net</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services" onclick="serviceBoxAnim()">Sedang Populer</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about" onclick="aboutBoxAnim()">Tentang Kami</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact" onclick="contactBoxAnim()">Kontak</a></li>
+                        <li class="nav-item"><a class="nav-link" href="User_Regist/signIn.php">Masuk</a></li>
+                        <li class="nav-item"><a class="nav-link" href="User_Regist/signUp.php">Daftar</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <header class="pagehead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <div class="col-12 text-center">
+                        <h1 class="font-weight-semibold ml3 fadeInUp">Sebuah Blog Dimana Inspirasi Datang</h1>
+                        <hr class="divider light my-4">
+                        <p class="subHeader fadeInDown">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <a href="#about" class="btn btn-xl js-scroll-trigger" role="button" id="btn_about">Tentang Kami</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!--Services-->
+        <section class="page-section" id="services" style="padding-top: 90px; padding-bottom:40px">
+            <div class="container">
+                <h2 class="text-center mt-0"><i class="fas fa-chart-line"></i> Sedang Populer</h2>
+                <hr class="divider my-4" />  
+                <div class="d-flex flex-column bd-highlight mb-3"> <!--coloumn-->
+                    <!--row 1-->
+                    <div class="p-2 bd-highlight">  
+                        <div class="d-flex flex-row bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                <div class="d-flex flex-column bd-highlight mb-3">
+                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Kang Buncis</div>
+                                    <div class="p-2 bd-highlight">
+                                        <h5 class="font-weight-bold">Kode Yang Baik Ialah Kode Yang Mudah Dibaca Oleh Orang Lain</h5>
+                                    </div>
+                                    <div class="p-2 bd-highlight">
+                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <div class="d-flex flex-column bd-highlight mb-3">
+                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Abah Tua</div>
+                                    <div class="p-2 bd-highlight">
+                                        <h5 class="font-weight-bold">Bolehkah Menunda?</h5>
+                                    </div>
+                                    <div class="p-2 bd-highlight">
+                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <div class="d-flex flex-column bd-highlight mb-3">
+                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Ariq Fachry</div>
+                                    <div class="p-2 bd-highlight">
+                                        <h5 class="font-weight-bold">Bitcoin Punya Nilai Intrinsik?</h5>
+                                    </div>
+                                    <div class="p-2 bd-highlight">
+                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    <!--row 2-->
+                    <div class="p-2 bd-highlight">
+                        <div class="d-flex flex-row bd-highlight mb-3">
+                            <div class="p-2 bd-highlight">
+                                <div class="d-flex flex-column bd-highlight mb-3">
+                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Kang Buncis</div>
+                                    <div class="p-2 bd-highlight">
+                                        <h5 class="font-weight-bold">Kode Yang Baik Ialah Kode Yang Mudah Dibaca Oleh Orang Lain</h5>
+                                    </div>
+                                    <div class="p-2 bd-highlight">
+                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <div class="d-flex flex-column bd-highlight mb-3">
+                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Abah Tua</div>
+                                    <div class="p-2 bd-highlight">
+                                        <h5 class="font-weight-bold">Bolehkah Menunda?</h5>
+                                    </div>
+                                    <div class="p-2 bd-highlight">
+                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <div class="d-flex flex-column bd-highlight mb-3">
+                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Uang Farmer</div>
+                                    <div class="p-2 bd-highlight">
+                                        <h5 class="font-weight-bold">Bitcoin Punya Nilai Intrinsik?</h5>
+                                    </div>
+                                    <div class="p-2 bd-highlight">
+                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>                    
+                </div>
+                      
+            </div>
+        </section>
+        <!--about-->
+        <section class="about" id="about">
+            <div class="container h-100" id="container_about" >
+                <div class="row h-100 align-items-center">
+                    <div class="col-12 text-center">
+                        <h2 class="text-white mt-0">Semua Inspirasi Perlu Ringkas!</h2>
+                        <hr class="divider light my-4" style="border-color:white">
+                        <p class="mb-4" style="color: #EEEAEA;">Sebuah Blog Dimana User Dapat Menulis Dan Berbagi Pengalaman dan Pengetahuan Satu Sama Lainnya<br> Ayo Mulai Menulis dan Saling Berbagi!</p>
+                        <button type="button" class="btn btn-xl" id="btn_getStart" onclick="showAlert()">Mulai</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--Contact-->
+        <section class="page-section" id="contact" >
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <div class="col-lg-8 text-center">
+                        <h2 class="mt-0" id="contactHead">Let's Get In Touch!</h2>
+                        <hr class="divider my-4" />
+                        <p class="mb-5" id="contactLead" style="color: #D7CCCC;">Punya Pertanyaan, Kritik, Dan Saran Untuk Kami? Kami Akan Selalu Menanti Pesan-Pesan Dari Anda Melalui Kontak Dibawah!</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                        <i class="fas fa-phone fa-3x mb-3 icontact"></i>
+                        <div>+62-238-83-122</div>
+                    </div>
+                    <div class="col-lg-4 mr-auto text-center">
+                        <i class="fas fa-envelope fa-3x mb-3 icontact"></i>
+                        <a class="d-block text-decoration-none" style="color: white;">project.ringkasnet@gmail.com</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container">
+                <div class="small text-center text-muted">
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script>
+                    Project Ringkas-Net
+                </div>
+            </div>
+        </footer>
+
+ 
+    <!--anime js-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+    <!--sweet alert-->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script> <!--cdn-->
+    <script src="sweetalert2.all.min.js"></script>
+    <!-- Optional: include a polyfill for ES6 Promises for IE11 -->
+    <script src="//cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.js"></script>
+    <script>
+        (function ($) {
+            "use strict"; // Start of use strict
+
+            // Smooth scrolling using anime.js
+            $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', function () {
+                if (
+                    location.pathname.replace(/^\//, "") ==
+                    this.pathname.replace(/^\//, "") &&
+                    location.hostname == this.hostname
+                ) {
+                    var target = $(this.hash);
+                    target = target.length ?
+                        target :
+                        $("[name=" + this.hash.slice(1) + "]");
+                    if (target.length) {
+                        anime({
+                            targets: 'html, body',
+                            scrollTop: target.offset().top - 72,
+                            duration: 1000,
+                            easing: 'easeInOutExpo'
+                        });
+                        return false;
+                    }
+                }
+            });
+
+            // Closes responsive menu when a scroll trigger link is clicked
+            $('.js-scroll-trigger').click(function () {
+                $('.navbar-collapse').collapse('hide');
+            });
+
+            // Activate scrollspy to add active class to navbar items on scroll
+            $('body').scrollspy({
+                target: '#mainNav',
+                offset: 75
+            });
+
+            // Collapse Navbar
+            var navbarCollapse = function () {
+                if ($("#mainNav").offset().top > 100) {
+                    $("#mainNav").addClass("navbar-scrolled");
+                    
+                } else {
+                    $("#mainNav").removeClass("navbar-scrolled");
+                }
+            };
+            // Collapse now if page is not at top
+            navbarCollapse();
+            // Collapse the navbar when page is scrolled
+            $(window).scroll(navbarCollapse);
+
+
+        })(jQuery); 
+        // function moveWindow(x, y){
+        //     window.scrollTo(x,y);
+        // }
+        function showAlert(){
+            Swal.fire({
+                icon: 'question',
+                title: 'Sudah Memiliki Akun?',
+                showDenyButton: true,
+                confirmButtonText: `Sudah`,
+                denyButtonText: `Belum`,
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    window.location = "User_Regist/signIn.php";
+                } else if (result.isDenied) {
+                    Swal.fire({
+                    icon: 'question',
+                    title: 'Ingin Mendaftar?',
+                    showCancelButton: true,
+                    confirmButtonText: `Daftar`,
+                    CancelButtonText: `Nanti Saja`,
+                    }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location = "User_Regist/signUp.php";
+                    } 
+                    })
+                }
+                })
+        }  
+        //function box animation when nav-item clicked
+        function serviceBoxAnim(){
+            var element = document.getElementById("services");
+            element.classList.add("fadeInDown");
+        }
+        function aboutBoxAnim(){
+            var element = document.getElementById("about");
+            element.classList.add("fadeInUp");
+        }
+        function contactBoxAnim(){
+            var elementHead = document.getElementById("contactHead");
+            var elementLead = document.getElementById("contactLead");
+            elementHead.classList.add("fadeInUp");
+            elementLead.classList.add("fadeInDown");
+        }     
+    </script>
+    </body>
+</html>
+    
