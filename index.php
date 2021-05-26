c0d344f57a1d3a15853cbc6c2f5b30045b0f9dad
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-05-26 11:01:25 +0700

    add web icon

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
     <head>
-        <title>AdoptMe | Home</title>
+        <title>Ringkas.Net - Dimana Inspirasi Ringkas Bisa Didapatkan</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- icon -->
-        <link rel="icon" type="image/png" href="src/img/adoptMe.png">
+        <link rel="icon" type="image/png" href="src/img/logo-ringkasnet.png">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
