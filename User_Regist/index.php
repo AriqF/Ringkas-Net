8c52acbb264af94532578a350a9eaff808ba25c1
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-05-27 14:05:00 +0700

    update 404 page design

--- User_Regist/index.php
+++ User_Regist/index.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	 <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<title>404</title>
-</head>
-<body>
-
-	<div class="container">
-	<div class="row">
-		<div class="col-md-6 mr-auto ml-auto text-center py-5 mt-5">
-			<div class="card">
-				<div class="card-body">
-					<h1 class="card-title bg-warning"> Error Page</h1>
-					<h2 class="card-title"> 404 Error</h2>
-					<img src="../src/img/giphy.gif" width="300px" height="300px">
-					<p class="card-text"> The page you are searching for is not available, 
-					Please go back to your previous page.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-	
-</body>
-</html>
-
+<?php
+	include '404.php';
+?>
