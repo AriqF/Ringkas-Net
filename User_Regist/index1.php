e177f586604e231efe66ea08a177745fff0911ad
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-05-27 18:40:44 +0700

    Update index1.php

--- User_Regist/index1.php
+++ User_Regist/index1.php
@@ -23,38 +23,10 @@ if (!isset($_SESSION['uid'])) {
 <!DOCTYPE html>
 <html>
     <head>
-        <title>MoKarya || Homepage</title>
-         <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-      
-        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-        
-        <!--Swiper JS-->
-        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
-        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-
-        <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
-        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-
-        <!--My css-->
-        <link href="../src/css/registerStyle.css" type="text/css" rel="stylesheet"> 
-
-        <!--Swiper.js-->
-        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
-        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-         
-        <!--Google Fonts-->
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Epilogue&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
-    </head>
-    <body>
+        <title>Ringkas.Net - Verifikasi</title>
+        <?php
+            include 'header-signin.php';
+        ?>
         <!-- <form form class="form" action="reset_password.php" method="post"> -->
         <div class="content">
             <div class="container" id="box" style="padding: 0; margin-top: 20px;">
@@ -100,5 +72,8 @@ if (!isset($_SESSION['uid'])) {
             </div>
         </div>
       <!--   </form> -->
+      <script>
+            document.getElementById("pictBox").style.backgroundImage = "url(../src/img/signin-img.jpg)";
+        </script>
     </body>
 </html>
