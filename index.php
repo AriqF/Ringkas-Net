53fbc677e75d06fd5532865f8b3d38850ac34f84
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-05-27 22:10:29 +0700

    update index design

--- index.php
+++ index.php
@@ -32,8 +32,8 @@
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services" onclick="serviceBoxAnim()">Sedang Populer</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about" onclick="aboutBoxAnim()">Tentang Kami</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services" onclick="serviceBoxAnim()">Sedang Populer</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact" onclick="contactBoxAnim()">Kontak</a></li>
                         <li class="nav-item"><a class="nav-link" href="User_Regist/signIn">Masuk</a></li>
                         <li class="nav-item"><a class="nav-link" href="User_Regist/signUp">Daftar</a></li>
@@ -53,97 +53,11 @@
                 </div>
             </div>
         </header>
-        <!--Services-->
-        <section class="page-section" id="services" style="padding-top: 90px; padding-bottom:40px">
-            <div class="container">
-                <h2 class="text-center mt-0"><i class="fas fa-chart-line"></i> Sedang Populer</h2>
-                <hr class="divider my-4" />  
-                <div class="d-flex flex-column bd-highlight mb-3"> <!--coloumn-->
-                    <!--row 1-->
-                    <div class="p-2 bd-highlight">  
-                        <div class="d-flex flex-row bd-highlight mb-3">
-                            <div class="p-2 bd-highlight">
-                                <div class="d-flex flex-column bd-highlight mb-3">
-                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Kang Buncis</div>
-                                    <div class="p-2 bd-highlight">
-                                        <h5 class="font-weight-bold">Kode Yang Baik Ialah Kode Yang Mudah Dibaca Oleh Orang Lain</h5>
-                                    </div>
-                                    <div class="p-2 bd-highlight">
-                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-2 bd-highlight">
-                                <div class="d-flex flex-column bd-highlight mb-3">
-                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Abah Tua</div>
-                                    <div class="p-2 bd-highlight">
-                                        <h5 class="font-weight-bold">Bolehkah Menunda?</h5>
-                                    </div>
-                                    <div class="p-2 bd-highlight">
-                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-2 bd-highlight">
-                                <div class="d-flex flex-column bd-highlight mb-3">
-                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Ariq Fachry</div>
-                                    <div class="p-2 bd-highlight">
-                                        <h5 class="font-weight-bold">Bitcoin Punya Nilai Intrinsik?</h5>
-                                    </div>
-                                    <div class="p-2 bd-highlight">
-                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>  
-                    </div>
-                    <!--row 2-->
-                    <div class="p-2 bd-highlight">
-                        <div class="d-flex flex-row bd-highlight mb-3">
-                            <div class="p-2 bd-highlight">
-                                <div class="d-flex flex-column bd-highlight mb-3">
-                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Kang Buncis</div>
-                                    <div class="p-2 bd-highlight">
-                                        <h5 class="font-weight-bold">Kode Yang Baik Ialah Kode Yang Mudah Dibaca Oleh Orang Lain</h5>
-                                    </div>
-                                    <div class="p-2 bd-highlight">
-                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-2 bd-highlight">
-                                <div class="d-flex flex-column bd-highlight mb-3">
-                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Abah Tua</div>
-                                    <div class="p-2 bd-highlight">
-                                        <h5 class="font-weight-bold">Bolehkah Menunda?</h5>
-                                    </div>
-                                    <div class="p-2 bd-highlight">
-                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-2 bd-highlight">
-                                <div class="d-flex flex-column bd-highlight mb-3">
-                                    <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Uang Farmer</div>
-                                    <div class="p-2 bd-highlight">
-                                        <h5 class="font-weight-bold">Bitcoin Punya Nilai Intrinsik?</h5>
-                                    </div>
-                                    <div class="p-2 bd-highlight">
-                                        <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> 
-                    </div>                    
-                </div>
-                      
-            </div>
-        </section>
         <!--about-->
-        <section class="about" id="about">
-            <div class="container h-100" id="container_about" >
-                <div class="row h-100 align-items-center">
-                    <div class="col-12 text-center">
+        <section class="about" id="about" style="padding-top: 60px; padding-bottom:80px">
+            <div class="container" >
+                <div class="row align-items-center">
+                    <div class="col-12 align-middle text-center">
                         <h2 class="text-white mt-0">Semua Inspirasi Perlu Ringkas!</h2>
                         <hr class="divider light my-4" style="border-color:white">
                         <p class="mb-4" style="color: #EEEAEA;">Sebuah Blog Dimana User Dapat Menulis Dan Berbagi Pengalaman dan Pengetahuan Satu Sama Lainnya<br> Ayo Mulai Menulis dan Saling Berbagi!</p>
@@ -152,8 +66,67 @@
                 </div>
             </div>
         </section>
+        <!--Trending-->
+        <section class="page-section" id="services" style="padding-top: 60px; padding-bottom:20px">
+            <div class="container-fluid">
+                <div class="px-lg-5">
+                    <h2 class="text-center mt-0"><i class="fas fa-chart-line"></i> Sedang Populer</h2>
+                    <hr class="divider my-4" style="margin-bottom: 16px;"/>  
+                    <div class="row">
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-4 anim">
+                            <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Kang Buncis</div>
+                            <div class="p-2 bd-highlight">
+                                <h5 class="font-weight-bold"><a href="#" class="title">Monitor Terbaik Untuk Coding</a></h5>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-4 anim">
+                            <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Kang Buncis</div>
+                            <div class="p-2 bd-highlight">
+                                <h5 class="font-weight-bold"><a href="#" class="title">Monitor Terbaik Untuk Coding</a></h5>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-4 anim">
+                            <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Kang Buncis</div>
+                            <div class="p-2 bd-highlight">
+                                <h5 class="font-weight-bold"><a href="#" class="title">Monitor Terbaik Untuk Coding</a></h5>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-4 anim">
+                            <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Kang Buncis</div>
+                            <div class="p-2 bd-highlight">
+                                <h5 class="font-weight-bold"><a href="#" class="title">Monitor Terbaik Untuk Coding</a></h5>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-4 col-md-6 mb-4 anim">
+                            <div class="p-2 bd-highlight"><i class="fas fa-user-tie"></i> Kang Buncis</div>
+                            <div class="p-2 bd-highlight">
+                                <h5 class="font-weight-bold"><a href="#" class="title">Monitor Terbaik Untuk Coding</a></h5>
+                            </div>
+                            <div class="p-2 bd-highlight">
+                                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                            </div>
+                        </div>
+                        
+  
+                    </div> <!--row div-->
+                </div> <!--px-lg-5 div-->
+            </div> <!--container-fluid-->
+        </section>
+
         <!--Contact-->
-        <section class="page-section" id="contact" >
+        <section class="page-section" id="contact" style="padding-top: 70px; padding-bottom:70px">
         <div class="container">
             <div class="row justify-content-center align-items-center">
                 <div class="col-lg-8 text-center">
