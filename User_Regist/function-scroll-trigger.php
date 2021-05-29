00747e066c79b3093661da4f0ad9c6c2603d752d
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-05-29 22:15:20 +0700

    Create function-scroll-trigger.php

---
+++ User_Regist/function-scroll-trigger.php
@@ -0,0 +1,4 @@
+<!--anime js-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+<script src="../src/js/scroll-trigger.js"></script>
