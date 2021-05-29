e07514e3223599662b880d342910aaebb2f3e4a0
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-05-29 22:15:25 +0700

    Update footer.php

--- User_Regist/footer.php
+++ User_Regist/footer.php
@@ -1,5 +1,5 @@
 <!-- Footer-->
-<footer class="py-3" style="background-color: #343a40;">
+<footer class="py-3 bg-dark footer">
     <div class="container">
         <div class="small text-center text-white">
             Copyright &copy;
@@ -10,7 +10,6 @@
         </div>
     </div>
 </footer>
-<!--anime js-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
-<script src="../src/js/scroll-trigger.js"></script>
+<?php 
+    include'function-scroll-trigger.php';
+?>
