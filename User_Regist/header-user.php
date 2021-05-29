af634759e4ae4340717c30d2e7d1a33a0e72f4ea
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-05-29 10:04:57 +0700

    Update header-user.php

--- User_Regist/header-user.php
+++ User_Regist/header-user.php
@@ -1,12 +1,9 @@
-<html>
-    <head>
-        <title>Home</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- icon -->
-        <link rel="icon" type="image/png" href="src/img/adoptMe.png">
+        <link rel="icon" type="image/png" href="../src/img/logo-ringkasnet.png">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
@@ -16,29 +13,51 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
         <!--My css-->
-        <link href="src/css/indexStyle.css" type="text/css" rel="stylesheet"> 
-        <link href="src/css/animation.css" type="text/css" rel="stylesheet"> 
+        <link href="../src/css/userStyle.css" type="text/css" rel="stylesheet"> 
+        <link href="../src/css/navbarStyle.css" type="text/css" rel="stylesheet"> 
+        <link href="../src/css/animation.css" type="text/css" rel="stylesheet"> 
         
         <!--Google Fonts-->
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Epilogue&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Benne&family=Overpass&family=Poppins&display=swap" rel="stylesheet">
 
         <script src="https://kit.fontawesome.com/babb4f3fd7.js" crossorigin="anonymous"></script>
     </head>
     <body id="page-top">
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top" onclick="headerPageAnim()">AdoptMe</a>
+                <a class="navbar-brand js-scroll-trigger" href="#page-top" style="font-family: 'Benne', serif; font-size:22px; letter-spacing:1px">Ringkas.Net</a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="User_Regist/signIn.php"><i class="fas fa-home"></i> Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="User_Regist/signUp.php"><i class="fas fa-paw"></i> Pet</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Profile</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact"><i class="fas fa-sign-out-alt"></i> Sign Out</a></li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php if($currentPage =='user_home'){echo 'active';}?>" href="user-dashboard">Home</a>
+                    </li>
+                    <li class="nav-item dropdown <?php if($currentPage =='write' || $currentPage == 'myBlog'){echo 'active';}?>">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Blog Saya
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item <?php if($currentPage =='write'){echo 'active';}?>" href="admin-gallery-data">Menulis</a>
+                            <a class="dropdown-item <?php if($currentPage =='myBlog'){echo 'active';}?>" href="admin-unggah">Blog Saya</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php if($currentPage =='user_profile'){echo 'active';}?>" href="user-profile">Profil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php if($currentPage =='user_rate'){echo 'active';}?>" href="user-rating">Feedback</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index1?logout=1">Keluar</a>
+                    </li>
                     </ul>
                 </div>
             </div>
         </nav>
+        <script>
+            $(".nav .nav-link").on("click", function(){
+            $(".nav").find(".active").removeClass("active");
+            $(this).addClass("active");
+            });
+        </script>
