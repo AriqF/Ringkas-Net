ce10ded2c3d1ad7ef3d612b27afddbc8f1b3eefc
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-06-01 09:55:36 +0700

    add query for blog

--- User_Regist/user-read-blog.php
+++ User_Regist/user-read-blog.php
@@ -4,22 +4,46 @@
     <?php
         $currentPage = 'user_read_blog'; //rubah sesuai dgn nama halaman, dan tambahkan juga dalam navbar di file header
         include 'header-user.php';
+
+        // mengecek apakah di url ada nilai GET id
+        if (isset($_GET['id'])) {
+            // ambil nilai id dari url dan disimpan dalam variabel $id
+            $id = ($_GET['id']);
+
+            // menampilkan data dari database yang mempunyai id=$id
+            $query = "SELECT * FROM blog WHERE id_blog='$id'";
+            $result = mysqli_query($conn, $query);
+            // jika data gagal diambil maka akan tampil error berikut
+            if(!$result){
+            die ("Query Error: ".mysqli_errno($conn).
+                " - ".mysqli_error($conn));
+            }
+            // mengambil data dari database
+            $data = mysqli_fetch_assoc($result);
+            // apabila data tidak ada pada database maka akan dijalankan perintah ini
+            if (!count($data)) {
+                echo "<script>alert('Data tidak ditemukan pada database');window.location='admin-gallery-data';</script>";
+            }
+        } else {
+            // apabila tidak ada data GET id pada akan di redirect ke admin-unggah
+            echo "<script>alert('Masukkan data id.');window.location='admin-gallery-data';</script>";
+        }         
+  ?>
     ?>
     <link href="../src/css/blogStyle.css" type="text/css" rel="stylesheet"> 
 
 
     <header class="blog-header text-center">
-        <h1 class="blog-title">Blog Title</h1>
-        <p class="lead" style="font-size: 16px;"><span style="color: #24C157; font-size: 17px;">#penulis</span> &#8231; 22/01/87</p>
+        <h1 class="blog-title"><?php echo $data['judul']; ?></h1>
+        <p class="lead" style="font-size: 16px;"><span style="color: #24C157; font-size: 17px;"><?php echo $data['penulis']; ?></span> &#8231; 22/01/87</p>
         <hr class="divider">
     </header>
     <div class="page-section blog-page">
 
         <div class="container">
-            <img src="../src/img/index-bg.jpg" class="img-fluid rounded">
+            <img src="image/<?php echo $data['gambar']; ?>" class="img-fluid rounded">
             <p class="blog-content">
-                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-                "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+                <?php echo $data['isi_blog']; ?>
             </p>
         </div>
 
