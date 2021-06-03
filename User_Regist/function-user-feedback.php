bd79eb48b0758b35ed78be7f42d8c2876450fba4
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-06-03 14:38:46 +0700

    create feedback page and function

---
+++ User_Regist/function-user-feedback.php
@@ -0,0 +1,22 @@
+<?php
+    require 'config/db.php';
+
+    $rating  = $_POST['rating'];
+    $komentar = $_POST['komentar'];
+
+    if (isset($_POST['anonymSend'])){
+        $username = "anonim";
+    }
+    else if(!isset($_POST['anonymSend'])){
+        $username = $_POST['username'];
+    }
+   
+    $query = "INSERT INTO rating (rating, komentar, nama_penilai) VALUES ('$rating', '$komentar', '$username')";
+    $result = mysqli_query($conn, $query);
+    if(!$result){
+        die ("Query gagal dijalankan: ".mysqli_errno($conn).
+            " - ".mysqli_error($conn));
+    } else {
+        echo "<script>alert('Terima Kasih Atas Feedback Anda!');window.location='user-feedback';</script>";
+    }
+?>
--- User_Regist/user-feedback.php
+++ User_Regist/user-feedback.php
@@ -4,6 +4,7 @@
     <?php
         $currentPage = 'user_rate'; 
         include 'header-user.php';
+        $uid = $_SESSION['uid'];
     ?>
     <header class="pagehead" id="user_write">
         <div class="container h-100">
@@ -23,64 +24,84 @@
                 <h3 style="margin-bottom: 12px" class="align-items-center fadeInUp text-center">Bagaimana Pengalaman Anda Menggunakan Sistem Kami?</h3>
                 <p style="font-size: 14px;"></p>
                 <hr class="divider mx-auto">
-                <form method="POST" class="rating" id="rateForm" enctype="multipart/form-data">
+                <form method="POST" class="rating" action="function-user-feedback" id="rateForm" enctype="multipart/form-data">
                     <div class="text-center">
                         <div class="form-check form-check-inline">
                             <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio1" style="cursor:pointer" name="customRadio" class="custom-control-input">
+                                <input type="radio" id="customRadio1" style="cursor:pointer" name="rating" class="custom-control-input" value="1">
                                 <label class="custom-control-label" for="customRadio1">Sangat Buruk</label>
                             </div>
                         </div>
                         <div class="form-check form-check-inline">
                             <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
+                                <input type="radio" id="customRadio2" name="rating" class="custom-control-input" value="2">
                                 <label class="custom-control-label" for="customRadio2">Buruk</label>
                             </div>
                         </div>
                         <div class="form-check form-check-inline">
                             <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio3" name="customRadio" class="custom-control-input">
+                                <input type="radio" id="customRadio3" name="rating" class="custom-control-input" value="3">
                                 <label class="custom-control-label" for="customRadio3">Netral</label>
                             </div>
                         </div>
                         <div class="form-check form-check-inline">
                             <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio4" name="customRadio" class="custom-control-input">
+                                <input type="radio" id="customRadio4" name="rating" class="custom-control-input" value="4">
                                 <label class="custom-control-label" for="customRadio4">Baik</label>
                             </div>
                         </div>
                         <div class="form-check form-check-inline">
                             <div class="custom-control custom-radio">
-                                <input type="radio" id="customRadio5" name="customRadio" class="custom-control-input">
+                                <input type="radio" id="customRadio5" name="rating" class="custom-control-input" value="5">
                                 <label class="custom-control-label" for="customRadio5">Sangat Baik</label>
                             </div>
                         </div>
-
                     </div>
-
+                    <input type="text" class="form-control" name="username" value="<?php echo $_SESSION['username']; ?> " hidden>
                     <div class="w-100"></div>
-                    <label class="label control-label" style="margin-top: 20px;">Komentar</label>
-                    <textarea type="text" class="form-control mb-3" name="komentar" rows="3"></textarea>
+                    <label class="label control-label" style="margin-top: 20px;"><span id="comments">Komentar</span></label>
+                    <textarea type="text" class="form-control mb-3" name="komentar" id="box_comments" rows="3" placeholder="Komentar anda mengenai sistem kami?"></textarea>
                     <div class="custom-control custom-checkbox" style="margin-top: 12px;">
-                        <input type="checkbox" class="custom-control-input" id="defaultChecked2" style="cursor:pointer" name="anonymSend" value="true">
-                        <label class="custom-control-label" for="defaultChecked2" id="keeptxt">Kirim sebagai anonim</label>
+                        <abbr title="Jika anda mencentang opsi ini, sistem tidak akan mengirim informasi identitas anda">
+                            <input type="checkbox" class="custom-control-input" id="defaultChecked2" style="cursor:pointer" name="anonymSend" value="true">
+                            <label class="custom-control-label" for="defaultChecked2" id="keeptxt">Kirim sebagai anonim</label>
+                        </abbr>
                     </div>
-                    <button name="submit-btn" type="submit" class="btn btn-upload" data-toggle="modal" data-target="#exampleModal">
+                    <button name="submit-btn" type="submit" class="btn btn-upload">
                         Kirim
                     </button>
                 </form>
             </div>
         </div>
     </section>
-
-    
+  
     <?php 
         include'function-scroll-trigger.php';
     ?>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
         //document.getElementById("user_write").style.backgroundImage = "url(../src/img/user-write-img.jpg)";
         $(':radio').change(function() {
-            console.log('New star rating: ' + this.value);
+            /* console.log('New star rating: ' + this.value); */
+            switch(this.value){
+                case "1":
+                    document.getElementById("box_comments").placeholder  = "Masalah apa yang anda dapatkan dalam menggunakan sistem kami?";
+                    break;
+                case "2":
+                    document.getElementById("box_comments").placeholder  = "Masalah apa yang anda dapatkan dalam menggunakan sistem kami?";
+                    break;
+                case "3":
+                    document.getElementById("box_comments").placeholder  = "Apa ada hal yang harus kami tingkatkan?";
+                    break;
+                case "4":
+                    document.getElementById("box_comments").placeholder  = "Apa ada hal yang harus kami tingkatkan?";
+                    break;
+                case "5":
+                    document.getElementById("box_comments").placeholder  = "Ceritakan Pengalaman Terbaik Anda..";
+                    break;
+                default:
+                    document.getElementById("box_comments").placeholder  = "Komentar anda mengenai sistem kami?";
+            }
         });
         document.getElementById("user_write").style.backgroundImage = "url(../src/img/user-feedback-img.jpg)";
     </script>
